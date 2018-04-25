f3fffcdffae09df48b0ef07ceef90b0976c75583
Ivon Gottgens <ivon@alientrick.com>
2018-04-25 12:17:01 +0200

    Caching

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ class Application {
         this.CACHE_NAME = 'pwa-cache-v1';
         this.urlsToCache = [
             './',
-            './build/css/styles.css'
+            './build/css/styles.css',
+            './assets/img/'
         ];
 
         self.addEventListener('install', this.onInstall);
-        // self.addEventListener('fetch', this.onFetch);
+        self.addEventListener('fetch', this.onFetch);
 
         that = this;
     }
 
     onInstall(event) {
         event.waitUntil(
-            caches.open(this.CACHE_NAME)
-                .then(function(cache) {
+            caches.open(that.CACHE_NAME)
+                .then(function (cache) {
                     console.log('Opened cache');
                     return cache.addAll(that.urlsToCache);
                 }
@@ -32,11 +33,30 @@ class Application {
     onFetch(event) {
         event.respondWith(
             caches.match(event.request)
-                .then(function(response) {
+                .then(function (response) {
                     if (response) {
                         return response;
                     }
-                    return fetch(event.request);
+
+                    var fetchRequest = event.request.clone();
+
+                    return fetch(fetchRequest)
+                        .then(function (response) {
+                            if (!response || response.status !== 200 || response.type !== 'basic') {
+                                return response;
+                            }
+
+                            var responseToCache = response.clone();
+
+                            caches.open(that.CACHE_NAME)
+                                .then(function(cache) {
+                                    cache.put(event.request, responseToCache);
+                                }
+                            );
+
+                            return response;
+                        }
+                    );
                 }
             )
         );
