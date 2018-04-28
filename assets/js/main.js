4a2233d9d70d3c6a020ec82b2056bcb36211bfe2
Ivon Gottgens <ivongottgens@gmail.com>
2018-04-28 15:09:40 +0200

    added badge

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,7 @@ function ShowNotification(title, message) {
     navigator.serviceWorker.getRegistration().then(function (reg) {
         reg.showNotification(title, {
             icon: './assets/img/icon-96.png',
+            badge: './assets/img/icon-96.png',
             body: message,
             vibrate: [200, 100, 200, 100, 200, 100, 200],
             tag: 'test-notification'
